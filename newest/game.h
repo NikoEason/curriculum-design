d0944def7ee715ef13c93ce7e42eb013dccc889f
NikoEason <2077822385@qq.com>
2026-05-08 19:37:21 +0800

    游行功能已完善，最新版

---
+++ newest/game.h
@@ -0,0 +1,8 @@
+#ifndef GAME_H
+#define GAME_H
+void gameModule();
+
+void toLowerCase(char *str);
+int selectQuestionCount() ;
+int startGame(int questionCount) ;
+#endif // GAME_H
---
+++ newest/myheaders.h
@@ -0,0 +1,13 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include "menu.h"
+#include "dictionary.h"
+#include "login.h"  
+#include "game.h"
+
+#endif // MYHEADERS_H  
